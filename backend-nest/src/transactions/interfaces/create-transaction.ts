23dfa934353da37c99ae86af43370c46289396d2
rod <dev_rodts@outlook.com>
2025-02-18 12:52:27 -0300

    "feat: create transaction interface"

---
+++ backend-nest/src/transactions/interfaces/create-transaction.ts
@@ -0,0 +1,10 @@
+export interface CreateTransactionInterface {
+    type: number;
+    date: string;
+    value: number;
+    cpf: string;
+    card: string;
+    time: string;
+    storeOwner: string;
+    storeName: string;
+}
