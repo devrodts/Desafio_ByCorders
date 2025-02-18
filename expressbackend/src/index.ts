2225b3b372cb8e51718bc6be5ee2d3c7918f8bb3
rod <dev_rodts@outlook.com>
2025-02-18 10:05:25 -0300

    "chore: init"

---
+++ expressbackend/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
