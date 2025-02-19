0d4268a90b4cee33e6d7dc9fbd628b83cf0c145a
rod <dev_rodts@outlook.com>
2025-02-19 09:09:34 -0300

    "refactor: refactor update user DTO"

--- backend-nest/src/users/dtos/update-user.dto.ts
+++ backend-nest/src/users/dtos/update-user.dto.ts
@@ -1,9 +1,6 @@
 export class UpdateUserDTO {
-    name?: string;
-    email?: string;
-    password?: string;
-    confirmPassword?: string;
-    createdAt?: Date;
-    updatedAt?: Date;
-}
-
+    readonly username?: string;
+    readonly email?: string;
+    readonly password?: string;
+  }
+  
